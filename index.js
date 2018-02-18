597703ad540a42f4e5edebce8c2d5f6381fb69c5
boxuanlu <boxuan@ualberta.ca>
2018-02-17 21:06:05 -0700

    finished

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  sum = 0;
+  array.forEach(number){
+    if(Array.isArray(number)){
+      sum += sumItems(number);
+    } else {
+      sum += number;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
